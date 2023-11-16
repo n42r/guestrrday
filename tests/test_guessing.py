bfd7559cdd8cbcb07fb8622d6fcbcfd2a8fca00a
unknown <nasr.kasrin@gmail.com>
2023-11-16 10:58:58 +0100

    refactored guestrrday module

--- tests/test_guessing.py
+++ tests/test_guessing.py
@@ -2,11 +2,10 @@ import pytest
 import shutil
 from pathlib import Path
 	
-from guestrrday.guestrrday import year_guesser
+from guestrrday.guestrrday import guess_track, guess_by_tracklist, guess_by_dir
 from guestrrday.track import track
 
 def test_guess_all_succeed():
-	dg = year_guesser()
 	Path('tests/temp/').mkdir()
 	out = (1980, 'Derby')
 	Path('tests/temp/003. Skyy - High.m4a').touch()
@@ -15,13 +14,12 @@ def test_guess_all_succeed():
 	Path('tests/temp/028. The Roots - The Next Movement.m4a').touch()
 	tr2 = track('tests/temp/028. The Roots - The Next Movement.m4a')
 	
-	assert dg.guess_track(tr) == out
-	assert dg.guess_track(tr2) == out2
+	assert guess_track(tr) == out
+	assert guess_track(tr2) == out2
 	shutil.rmtree('tests/temp/')
 	
 
 def test_guess_all_fail():
-	dg = year_guesser()
 	Path('tests/temp/').mkdir()
 	inp = 'tests/temp/019. Bill Connors Trio A Pedal 1985.m4a'
 	Path(inp).touch()
@@ -30,12 +28,11 @@ def test_guess_all_fail():
 	Path(in2).touch()
 	tr2 = track(in2)
 	
-	assert dg.guess_track(tr) is None
-	assert dg.guess_track(tr2) is None
+	assert guess_track(tr) is None
+	assert guess_track(tr2) is None
 	shutil.rmtree('tests/temp/')
 	
 def test_guess_directory_all_succeed():
-	dg = year_guesser()
 	Path('tests/temp/').mkdir()
 	in1 = 'tests/temp/003. Skyy - High.m4a'
 	out = 'tests/temp/003. Skyy - High (Derby, 1980).m4a'
@@ -43,7 +40,7 @@ def test_guess_directory_all_succeed():
 	in2 = 'tests/temp/028. The Roots - The Next Movement.m4a'
 	out2 = 'tests/temp/028. The Roots - The Next Movement (MCA Records, 1999).m4a'
 	Path(in2).touch()
-	dg.guess_by_dir('tests/temp/')
+	guess_by_dir('tests/temp/')
 	
 	assert Path(out).exists()
 	assert Path(out2).exists()
@@ -51,7 +48,6 @@ def test_guess_directory_all_succeed():
 
 
 def test_guess_directory_all_fail():
-	dg = year_guesser()
 	Path('tests/temp/').mkdir()
 	in1 = 'tests/temp/019. Bill Connors Trio A Pedal 1985.m4a'
 	out = 'tests/temp/019. Bill Connors Trio A Pedal 1985.m4a'
@@ -59,7 +55,7 @@ def test_guess_directory_all_fail():
 	in2 = "tests/temp/096. Left Hand Frank and his Blues Band, Blues won't let me be"
 	out2 = "tests/temp/096. Left Hand Frank and his Blues Band, Blues won't let me be"
 	Path(in2).touch()
-	dg.guess_by_dir('tests/temp/')
+	guess_by_dir('tests/temp/')
 	
 	assert Path(out).exists() 
 	assert Path(out2).exists()
@@ -67,7 +63,6 @@ def test_guess_directory_all_fail():
 
 
 def test_guess_tracklist_all_succeed():
-	dg = year_guesser()
 	Path('tests/temp/').mkdir()
 	in1 = 'tests/temp/tracklist'
 	#Path(in1).touch()	
@@ -79,7 +74,7 @@ def test_guess_tracklist_all_succeed():
 	out1= in1 + '-guessed'
 	lines_out = ['tests/temp/003. Skyy - High (Derby, 1980).m4a', 'tests/temp/028. The Roots - The Next Movement (MCA Records, 1999).m4a']
 	
-	dg.guess_by_tracklist(in1)
+	guess_by_tracklist(in1)
 
 	idx = 0
 	with open(out1, encoding='utf8') as f:
@@ -91,7 +86,6 @@ def test_guess_tracklist_all_succeed():
 
 
 def test_guess_tracklist_all_fail():
-	dg = year_guesser()
 	Path('tests/temp/').mkdir()
 	in1 = 'tests/temp/tracklist'
 	#Path(in1).touch()	
@@ -103,7 +97,7 @@ def test_guess_tracklist_all_fail():
 	out1= in1 + '-guessed'
 	lines_out = ['tests/temp/019. Bill Connors Trio A Pedal 1985.m4a', "tests/temp/096. Left Hand Frank and his Blues Band, Blues won't let me be.m4a"]
 	
-	dg.guess_by_tracklist(in1)
+	guess_by_tracklist(in1)
 
 	idx = 0
 	with open(out1, encoding='utf8') as f:
@@ -114,10 +108,9 @@ def test_guess_tracklist_all_fail():
 	
 
 # def test_many_tracks():
-	# dg = year_guesser()
 	# global GUESS_LIST
 	# for i in GUESS_LIST:
 		# inp, out  = i
 		# tr = track(inp)
-		# assert dg.guess_track(tr) == out
+		# assert guess_track(tr) == out
 		
