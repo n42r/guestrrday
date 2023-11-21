c0f207b71552adf3bd2eaca9904616223c1b5d0a
unknown <nasr.kasrin@gmail.com>
2023-11-21 18:41:27 +0100

    pylint > 9

--- guestrrday/__main__.py
+++ guestrrday/__main__.py
@@ -2,16 +2,18 @@ import click
 from guestrrday.guestrrday import guess
 
 @click.command()
-@click.option('--input', required=True, help='Directory with music files or text file with tracklist, or comma separated list of tracks.')
+@click.option('--input', required=True,
+
+	help='Dir with music files or filename with tracklist, or comma separated list of tracks.')
 def main(input):
 	"""Tool for guessing the year and record label of music tracks
-	
+
 	Args:
-		input (str): Directory with music files or text file with tracklist, or comma separated list of tracks. Just give it what you need.
+		input (str): Directory with music files or text file with tracklist,
+
+			or comma separated list of tracks. Just give it what you need.
 	"""
 	return guess(input)
 
-
 if __name__ == "__main__":
 	main(input)
-
